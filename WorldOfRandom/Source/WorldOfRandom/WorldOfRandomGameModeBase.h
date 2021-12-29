44eea732515aa9b6248bfbb4254ef6fd46c361b4
Alfonso <alfonsotorres.schz@gmail.com>
2021-12-29 17:53:11 +0100

    Unreal Engine project creation

---
+++ WorldOfRandom/Source/WorldOfRandom/WorldOfRandomGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WorldOfRandomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WORLDOFRANDOM_API AWorldOfRandomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
